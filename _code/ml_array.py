33185c684722fbf5c6ba1084f4b984c5a0442467
Karan Jayachandra <karan.jayachandra@nxp.com>
2021-10-19 15:26:56 +0200

    Added to drafts instead of projects

--- _code/ml_array.py
+++ _code/ml_array.py
@@ -1,40 +1,41 @@
-import numpy
+from numpy import array, ones, abs, max, cumsum, log, linspace, pi, cos, exp
 import matplotlib.pyplot as plot
 
 mlaRefernce = {
-     "2" : numpy.array([1]),
-     "3" : numpy.array([1, 2]),
-     "4" : numpy.array([1, 3, 2]),
-     "5" : numpy.array([1, 3, 3, 2]),
-     "6" : numpy.array([1, 5, 3, 2, 2]),
-     "7" : numpy.array([1, 3, 6, 2, 3, 2]),
-     "8" : numpy.array([1, 3, 6, 6, 2, 3, 2]),
-     "9" : numpy.array([1, 3, 6, 6, 6, 2, 3, 2]),
-    "10" : numpy.array([1, 2, 3, 7, 7, 7, 4, 4, 1]),
-    "11" : numpy.array([1, 2, 3, 7, 7, 7, 7, 4, 4, 1])
+     "2" : array([1]),
+     "3" : array([1, 2]),
+     "4" : array([1, 3, 2]),
+     "5" : array([1, 3, 3, 2]),
+     "6" : array([1, 5, 3, 2, 2]),
+     "7" : array([1, 3, 6, 2, 3, 2]),
+     "8" : array([1, 3, 6, 6, 2, 3, 2]),
+     "9" : array([1, 3, 6, 6, 6, 2, 3, 2]),
+    "10" : array([1, 2, 3, 7, 7, 7, 4, 4, 1]),
+    "11" : array([1, 2, 3, 7, 7, 7, 7, 4, 4, 1])
 }
 
 def arrayFactor(spacing, angles):
-    angleEffect = numpy.cos(angles)
-    pattern = numpy.ones(angleEffect.size, dtype=numpy.complex)
+    angleEffect = cos(angles)
+    pattern = ones(angleEffect.size, dtype=complex)
     for elementSpacing in spacing:
-        elementEffect = numpy.exp(1j*numpy.pi*elementSpacing*angleEffect)
+        elementEffect = exp(1j*pi*elementSpacing*angleEffect)
         pattern += elementEffect
-    pattern = numpy.abs(pattern)
-    pattern /= numpy.max(pattern)
-    pattern = 20 * numpy.log(pattern)
+    pattern = abs(pattern)
+    pattern /= max(pattern)
+    pattern = 20 * log(pattern)
     return pattern
 
 elements = 8
-angles = numpy.linspace(-numpy.pi, numpy.pi, 1000)
-uniformSpacing = numpy.ones(elements - 1)
-uniformSpacing = numpy.cumsum(uniformSpacing)
-mlaSpacing = numpy.cumsum(mlaRefernce[str(elements)])
+angles = linspace(-pi, pi, 1000)
+uniformSpacing = ones(elements - 1)
+uniformSpacing = cumsum(uniformSpacing)
+mlaSpacing = cumsum(mlaRefernce[str(elements)])
 uniformElementPattern = arrayFactor(uniformSpacing, angles)
 mlaElementPattern = arrayFactor(mlaSpacing, angles)
 
 fig = plot.figure()
-fig.suptitle("Minimum Redundancy Linear Array (" + str(elements) + " Elements)", fontsize=20, weight=50)
+title = "Minimum Redundancy Linear Array (" + str(elements) + " Elements)"
+fig.suptitle(title, fontsize=20, weight=50)
 
 cartPlot = plot.subplot(121)
 cartPlot.plot(angles, uniformElementPattern)
---
+++ _drafts/2021-12-16-radar-phase-noise.md
@@ -0,0 +1,8 @@
+---
+layout: post
+title:  "Phase Noise in FMCW Radar Systems"
+date:   2021-11-02 14:37:00 +0200
+categories: radar
+---
+
+
